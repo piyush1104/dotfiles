f13a1681cbc996874e7cf28fb7688280ff9224b5
Piyush Bansal <19729997+piyush1104@users.noreply.github.com>
2021-08-15 21:23:30 +0530

    break zshrc into more

---
+++ zsh/.zsh/paths.zsh
@@ -0,0 +1,16 @@
+before_paths=(
+    # ~/.composer/vendor/bin
+    ~/bin
+)
+for p in ${before_paths[@]}; do
+    PATH=$p:$PATH
+done
+
+after_paths=(
+    # ~/.composer/vendor/bin
+)
+for p in ${after_paths[@]}; do
+    PATH=$PATH:$p
+done
+
+export PATH=$PATH
